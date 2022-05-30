f78c3f571b3b3b088a96fc4354a33d29e508cd95
Aleksey Obukhov <miptleha@gmail.com>
2022-05-30 22:02:52 +0300

    fix #3

---
+++ TextDecorator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace quick_asp_todo
+{
+    public class TextDecorator
+    {
+        public static string Decorate(string text)
+        {
+            text = HtmlToPlainText(text);
+            text = Linkify(text);
+            return text;
+        }
+
+        private static string HtmlToPlainText(string html)
+        {
+            const string tagWhiteSpace = @"(>|$)(\W|\n|\r)+<";//matches one or more (white space or line breaks) between '>' and '<'
+            const string stripFormatting = @"<[^>]*(>|$)";//match any character between '<' and '>', even when end tag is missing
+            const string lineBreak = @"<(br|BR)\s{0,1}\/{0,1}>";//matches: <br>,<br/>,<br />,<BR>,<BR/>,<BR />
+            var lineBreakRegex = new Regex(lineBreak, RegexOptions.Multiline);
+            var stripFormattingRegex = new Regex(stripFormatting, RegexOptions.Multiline);
+            var tagWhiteSpaceRegex = new Regex(tagWhiteSpace, RegexOptions.Multiline);
+
+            var text = html;
+            //Decode html specific characters
+            text = System.Net.WebUtility.HtmlDecode(text);
+            //Remove tag whitespace/line breaks
+            text = tagWhiteSpaceRegex.Replace(text, "><");
+            //Replace <br /> with line breaks
+            text = lineBreakRegex.Replace(text, Environment.NewLine);
+            //Strip formatting
+            text = stripFormattingRegex.Replace(text, string.Empty);
+
+            return text;
+        }
+
+        static string Linkify(string SearchText)
+        {
+            Regex regx = new Regex(@"(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+");
+            MatchCollection matches = regx.Matches(SearchText);
+            foreach (Match match in matches)
+            {
+                if (match.Value.StartsWith("http"))
+                    SearchText = SearchText.Replace(match.Value, "<a href='" + match.Value + "'>" + match.Value + "</a>");
+                else
+                    SearchText = SearchText.Replace(match.Value, "<a href='http://" + match.Value + "'>" + match.Value + "</a>");
+            }
+
+            return SearchText;
+        }
+    }
+}
